da5e7659ee9bb80c8172623a3ae66c6b7ada0e0e
Abdullah Ali <abdullahaliofc@gmail.com>
2024-12-02 14:16:45 -0800

    order_items.sql

---
+++ order_items.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `order_items` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `unit_price` decimal(4,2) NOT NULL,
+  PRIMARY KEY (`order_id`,`product_id`),
+  KEY `fk_order_items_products_idx` (`product_id`),
+  CONSTRAINT `fk
