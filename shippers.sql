5b65b5dd1fb099c6ce8d3697b0c651995a8bb816
Abdullah Ali <abdullahaliofc@gmail.com>
2024-12-02 14:12:28 -0800

    shippers.sql

---
+++ shippers.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `shippers` (
+  `shipper_id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`shipper_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `shippers` VALUES (1,'Hettinger LLC');
+INSERT INTO `shippers` VALUES (2,'Schinner-Predovic');
+INSERT INTO `shippers` VALUES (3,'Satterfield LLC');
+INSERT INTO `shippers` VALUES (4,'Mraz, Renner and Nolan');
+INSERT INTO `shippers` VALUES (5,'Waters, Mayert and Prohaska');
